2cc6a560843cb26e33dde02b1a60c455fa25d67b
Kaiky <kaikydiasnetto21@gmail.com>
2024-03-19 22:54:30 -0300

    get video url from zoom

--- src/main/java/com/example/zoomtovimeo/controller/WebhookController.java
+++ src/main/java/com/example/zoomtovimeo/controller/WebhookController.java
@@ -1,6 +1,9 @@
-package com.example.zoomtovimeo.controller;import com.example.zoomtovimeo.utils.HMACUtil;
+package com.example.zoomtovimeo.controller;
+
+import com.example.zoomtovimeo.utils.HMACUtil;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -41,12 +44,13 @@ public class WebhookController {
         } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().body("Erro ao fazer o parsing do JSON.");
-        }
-        System.out.println(bodyBuilder.toString());
+        }    
+
+        String event = payload.getEvent();
+        System.out.println("EVENTO: " +event);
 
         // Validando o campo 'event'
-        String event = payload.getEvent();
-        
+        if(!event.equals("endpoint.url_validation")) {
 
         // Extraindo o valor de plainToken do objeto Payload
         String plainToken = payload.getPayload().getPlainToken();
@@ -57,34 +61,62 @@ public class WebhookController {
         String hashForVerify = HMACUtil.generateHMAC(message, secretKey);
         
         String signature = "v0="+hashForVerify;
-
+        
         if(signature.equals(xZmSignature)) {
-            if (!"endpoint.url_validation".equals(event)) {
-                System.out.println("INVALID");
-                return ResponseEntity.badRequest().body("Evento inválido: " + event);
-            } else {
-
-                String hashForValidate = HMACUtil.generateHMAC(plainToken, secretKey);
-                
-                // Construindo o corpo de resposta JSON
+                    String hashForValidate = HMACUtil.generateHMAC(plainToken, secretKey);
+                    
                 Map<String, Object> responseMap = new LinkedHashMap<>();
-                Map<String, String> messageMap = new LinkedHashMap<>();
                 responseMap.put("plainToken", plainToken);
                 responseMap.put("encryptedToken", hashForValidate);
                 // responseMap.put("message", messageMap);
-    
-                // Convertendo o objeto JSON para uma string
+        
                 try {
                     String responseBody = objectMapper.writeValueAsString(responseMap);
-                    System.out.println(responseBody);
+                    System.out.println("Resposta JSON: " + responseBody); // Imprime a resposta JSON
                     return ResponseEntity.ok().body(responseBody);
                 } catch (IOException e) {
                     e.printStackTrace();
                     return ResponseEntity.badRequest().body("Erro ao gerar a resposta JSON.");
                 }
+           
+        } else if(event.equals("recording.completed")) {
+            // Acessando a lista de recording_files
+            List<RecordingFile> recordingFiles = payload.getPayload().getObject().getRecording_files();
+            
+            // Iterando sobre os arquivos de gravação
+            for (RecordingFile recordingFile : recordingFiles) {
+                // Verificando se o recording_type é igual ao valor desejado
+                if ("shared_screen_with_speaker_view".equals(recordingFile.getRecording_type())) {
+                    String downloadUrl = recordingFile.getDownload_url();
+                    
+                    System.out.println("Download URL do recording_type desejado: " + downloadUrl);
+                    
+                    // Retorna a resposta HTTP com sucesso
+                    return ResponseEntity.ok().body("Download URL do recording_type desejado: " + downloadUrl);
+                }
             }
+            
+            // Se nenhum arquivo de gravação com o recording_type desejado for encontrado
+            System.out.println("Nenhum arquivo de gravação com o recording_type desejado foi encontrado.");
+            
+            // Retorna a resposta HTTP com uma mensagem indicando que nenhum arquivo foi encontrado
+            return ResponseEntity.ok().body("Nenhum arquivo de gravação com o recording_type desejado foi encontrado.");
         } else {
-            return ResponseEntity.ok().body("Unauthorized!");
+            // ObjectMapper objectMapper2 = new ObjectMapper();
+            // String payloadContentJson;
+            // try {
+            //     payloadContentJson = objectMapper.writeValueAsString(payload.getPayload());
+            //     System.out.println("Payload JSON: " + payloadContentJson);
+            // } catch (JsonProcessingException e) {
+            //     e.printStackTrace();
+            //     // Trate a exceção adequadamente
+            // }
+            return ResponseEntity.ok().body("teste!");
+        }
+        } else {
+            System.out.println("event");
+            return ResponseEntity.ok().body("teste!");
+
         }
     }
 
@@ -95,6 +127,19 @@ public class WebhookController {
 
         @JsonProperty("payload")
         private PayloadContent payload;
+  
+        @JsonProperty("duration")
+        private PayloadContent duration;
+        
+       
+
+        public PayloadContent getDuration() {
+            return duration;
+        }
+
+        public void setDuration(PayloadContent duration) {
+            this.duration = duration;
+        }
 
         public String getEvent() {
             return event;
@@ -116,6 +161,29 @@ public class WebhookController {
     static class PayloadContent {
         @JsonProperty("plainToken")
         private String plainToken;
+        @JsonProperty("event")
+        private String event;
+        
+
+        @JsonProperty("object") // Ajuste para incluir o campo "event" dentro de "object"
+        private PayloadObject object;
+
+        
+        public String getAccount_id() {
+            return account_id;
+        }
+
+        @JsonProperty("account_id") // Ajuste para incluir o campo "event" dentro de "object"
+        private String account_id;
+
+        
+        public String getEvent() {
+            return event;
+        }
+
+        public void setEvent(String event) {
+            this.event = event;
+        }
 
         public String getPlainToken() {
             return plainToken;
@@ -124,5 +192,167 @@ public class WebhookController {
         public void setPlainToken(String plainToken) {
             this.plainToken = plainToken;
         }
+
+        public PayloadObject getObject() {
+            return object;
+        }
+
+        public void setObject(PayloadObject object) {
+            this.object = object;
+        }
     }
+
+    static class PayloadObject {
+        @JsonProperty("event") // Campo "event" dentro de "object"
+        private String event;
+
+        
+        @JsonProperty("duration")
+        private float duration;
+
+        @JsonProperty("type")
+        private int type;
+
+        
+        @JsonProperty("start_time")
+        private String start_time;
+        
+        
+        @JsonProperty("topic")
+        private String topic;
+
+        
+        @JsonProperty("timezone")
+        private String timezone;
+
+        
+        @JsonProperty("id")
+        private String id;
+
+        @JsonProperty("uuid")
+        private String uuid;
+
+        @JsonProperty("host_id")
+        private String host_id ;
+
+        
+        @JsonProperty("recording_file")
+        private RecordingFile recording_file;
+
+        
+    @JsonProperty("account_id")
+    private String account_id; // Adicionar esta linha
+
+        
+        @JsonProperty("account_file")
+        private RecordingFile account_file;
+
+        
+    @JsonProperty("host_email")
+    private String host_email;
+
+    
+    @JsonProperty("recording_count")
+    private Float recording_count;
+
+    
+    @JsonProperty("share_url")
+    private String share_url;
+
+ @JsonProperty("recording_files")
+    private List<RecordingFile> recording_files;
+
+
+    public List<RecordingFile> getRecording_files() {
+    return recording_files;
+}
+
+public void setRecording_files(List<RecordingFile> recording_files) {
+    this.recording_files = recording_files;
+}
+
+    @JsonProperty("total_size")
+    private Float total_size;
+
+    @JsonProperty("play_url")
+private String play_url;
+
+
+@JsonProperty("password")
+private String password;
+
+
+
+
+@JsonProperty("on_prem")
+private Boolean on_prem;
+
+@JsonProperty("recording_play_passcode")
+private String recording_play_passcode;
+
+        
+        
+        public float getDuration() {
+            return duration;
+        }
+
+        public void setDuration(float duration) {
+            this.duration = duration;
+        }
+        public String getEvent() {
+            return event;
+        }
+
+        public void setEvent(String event) {
+            this.event = event;
+        }
+    }
+    static class RecordingFile {
+        @JsonProperty("recording_start")
+        private String recording_start;
+        
+        @JsonProperty("recording_end")
+        private String recording_end;
+        
+        @JsonProperty("id")
+        private String id;
+        
+        @JsonProperty("meeting_id")
+        private String meeting_id;
+    
+        @JsonProperty("file_type")
+        private String file_type;
+        
+        @JsonProperty("file_extension")
+        private String file_extension;
+    
+        @JsonProperty("password")
+        private String password;
+    
+        @JsonProperty("file_size")
+        private String file_size;
+        
+        @JsonProperty("play_url")
+        private String play_url; // Adicionando a propriedade "play_url"
+    
+        @JsonProperty("download_url")
+        private String download_url; // Adicionando a propriedade "play_url"
+
+        public String getDownload_url() {
+            return download_url;
+        }
+
+        @JsonProperty("status")
+        private String status; // Adicionando a propriedade "play_url"
+
+        @JsonProperty("recording_type")
+        private String recording_type; // Adicionando a propriedade "play_url"
+        // Getters e setters
+
+        public String getRecording_type() {
+            return recording_type;
+        }
+    }
+    
+
 }
